4b11d694bf42fb5c5c13523c52c858b6b2cb2298
Your Name <you@example.com>
2025-01-28 13:47:47 +0530

    created new file

--- list_slices/slice1.py
+++ list_slices/slice1.py
@@ -1,9 +1,9 @@
 l=[1,2,3,4,5,6,7,8,9,10]
-print(l)
-print(type(l))
-print(l[0:5])
-print(l[0:8])
-print(l[0:4])
+print(l) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(type(l)) #<class 'list'>
+print(l[0:5]) #[1, 2, 3, 4, 5]
+print(l[0:8]) #[1, 2, 3, 4, 5, 6, 7, 8]
+print(l[0:4]) #
 print(l[0:7:2])
 print(l[4:9:3])
 print(l[2:6:8])
